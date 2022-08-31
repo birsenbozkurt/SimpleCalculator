b5996454368672cdd1d28ffaf5edcda49cd1fd98
birsenbozkurt <birsenbozkurt4041@gmail.com>
2022-08-31 17:41:37 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Anasayfa(),
+    );
+  }
+}
+
+class Anasayfa extends StatefulWidget {
+  const Anasayfa({Key? key}) : super(key: key);
+
+  @override
+  State<Anasayfa> createState() => _AnasayfaState();
+}
+
+class _AnasayfaState extends State<Anasayfa> {
+  String equation = "0";
+  String result = "0";
+  String expression = "";
+  double equationFontSize = 38.0;
+  double resultFontSize = 48.0;
+
+  buttonPressed(String buttonText) {
+    setState(() {
+      if (buttonText == "C") {
+        equation = "0";
+        result = "0";
+        equationFontSize = 38.0;
+        double resultFontSize = 48.0;
+      } else if (buttonText == "DEL") {
+        equationFontSize = 38.0;
+        resultFontSize = 48.0;
+        equation = equation.substring(0, equation.length - 1);
+        if (equation == "") {
+          equation = "0";
+        }
+      } else if (buttonText == "=") {
+        equationFontSize = 38.0;
+        resultFontSize = 48.0;
+
+        expression = equation;
+
+        try {
+          Parser p = Parser();
+          Expression exp = p.parse(expression);
+
+          ContextModel cm = ContextModel();
+          result = '${exp.evaluate(EvaluationType.REAL, cm)}';
+        } catch (e) {
+          result = "Error";
+        }
+      } else {
+        equationFontSize = 38.0;
+        resultFontSize = 48.0;
+        if (equation == "0") {
+          equation = buttonText;
+        } else {
+          equation = equation + buttonText;
+        }
+      }
+    });
+  }
+
+  Widget buildButton(String buttonText, double buttonHeight, Color buttonColor) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.1 * buttonHeight,
+      color: buttonColor,
+      child: FlatButton(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(0.0), side: BorderSide(color: Colors.white, width: 1, style: BorderStyle.solid)),
+          padding: EdgeInsets.all(16.0),
+          onPressed: () => buttonPressed(buttonText),
+          child: Text(
+            buttonText,
+            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.normal, color: Colors.white),
+          )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Calculator",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: Text(
+              "equation",
+              style: TextStyle(fontSize: equationFontSize),
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+            child: Text(
+              "result",
+              style: TextStyle(fontSize: resultFontSize),
+            ),
+          ),
+          Expanded(
+            child: Divider(),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width * .75,
+                child: Table(
+                  children: [
+                    TableRow(children: [
+                      buildButton("7", 1, Colors.black54),
+                      buildButton("8", 1, Colors.black54),
+                      buildButton("9", 1, Colors.black54),
+                    ]),
+                    TableRow(children: [
+                      buildButton("4", 1, Colors.black54),
+                      buildButton("5", 1, Colors.black54),
+                      buildButton("6", 1, Colors.black54),
+                    ]),
+                    TableRow(children: [
+                      buildButton("1", 1, Colors.black54),
+                      buildButton("2", 1, Colors.black54),
+                      buildButton("3", 1, Colors.black54),
+                    ]),
+                    TableRow(children: [
+                      buildButton("0", 1, Colors.black54),
+                      buildButton("00", 1, Colors.black54),
+                      buildButton("000", 1, Colors.black54),
+                    ]),
+                    TableRow(children: [
+                      buildButton("C", 1, Colors.redAccent),
+                      buildButton("DEL", 1, Colors.redAccent),
+                      buildButton("=", 1, Colors.green),
+                    ]),
+                  ],
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.25,
+                child: Table(
+                  children: [
+                    TableRow(children: [
+                      buildButton("+", 5, Colors.blue),
+                    ])
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
